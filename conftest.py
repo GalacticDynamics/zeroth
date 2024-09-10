43e41c7333f1d846cd7a75b9c4e4e6e47e6df57e
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-09-10 03:03:10 -0400

    build: bump optional_dependencies (#6)
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- conftest.py
+++ conftest.py
@@ -2,10 +2,11 @@
 
 import platform
 from doctest import ELLIPSIS, NORMALIZE_WHITESPACE
-from importlib.util import find_spec
 
 from sybil import Sybil
 from sybil.parsers.rest import DocTestParser, PythonCodeBlockParser, SkipParser
+
+from optional_dependencies import OptionalDependencyEnum, auto
 
 # TODO: stop skipping doctests on Windows when there is uniform support for
 #       numpy 2.0+ scalar repr. On windows it is printed as 1.0 instead of
@@ -25,12 +26,15 @@ pytest_collect_file = Sybil(
 ).pytest()
 
 
-# TODO: via separate optional_deps package
-HAS_ASTROPY = find_spec("astropy") is not None
-HAS_GALA = find_spec("gala") is not None
+class OptDeps(OptionalDependencyEnum):  # type: ignore[misc]  # pylint: disable=invalid-enum-extension
+    """Optional dependencies for ``zeroth``."""
+
+    ASTROPY = auto()
+    GALA = auto()
+
 
 collect_ignore_glob = []
-if not HAS_ASTROPY:
+if not OptDeps.ASTROPY.installed:
     collect_ignore_glob.append("src/unxt/_interop/unxt_interop_astropy/*")
-if not HAS_GALA:
+if not OptDeps.GALA.installed:
     collect_ignore_glob.append("src/unxt/_interop/unxt_interop_gala/*")
