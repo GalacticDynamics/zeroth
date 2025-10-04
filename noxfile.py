b3b74ee8d7946198889bf2dd35551455f918c6ac
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-10-04 17:47:34 -0400

    config: update (#27)
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- noxfile.py
+++ noxfile.py
@@ -1,52 +1,94 @@
 """Nox configuration."""
+# pylint: disable=import-error
 
 import shutil
 from pathlib import Path
 
 import nox
 
+nox.needs_version = ">=2024.3.2"
+nox.options.sessions = [
+    # Linting
+    "lint",
+    "precommit",
+    "pylint",
+    # Testing
+    "tests",
+]
+nox.options.default_venv_backend = "uv"
+
+
 DIR = Path(__file__).parent.resolve()
 
-nox.needs_version = ">=2024.3.2"
-nox.options.sessions = ["lint", "pylint", "tests"]
-nox.options.default_venv_backend = "uv|virtualenv"
+# =============================================================================
+# Linting
 
 
-@nox.session
-def lint(session: nox.Session) -> None:
+@nox.session(venv_backend="uv")
+def lint(session: nox.Session, /) -> None:
     """Run the linter."""
-    session.install("pre-commit")
-    session.run(
-        "pre-commit",
-        "run",
-        "--all-files",
-        "--show-diff-on-failure",
-        *session.posargs,
+    precommit(session)  # reuse pre-commit session
+    pylint(session)  # reuse pylint session
+
+
+@nox.session(venv_backend="uv")
+def precommit(session: nox.Session, /) -> None:
+    """Run pre-commit."""
+    session.run_install(
+        "uv",
+        "sync",
+        "--group=lint",
+        f"--python={session.virtualenv.location}",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
     )
+    session.run("pre-commit", "run", "--all-files", *session.posargs)
 
 
-@nox.session
-def pylint(session: nox.Session) -> None:
+@nox.session(venv_backend="uv")
+def pylint(session: nox.Session, /) -> None:
     """Run PyLint."""
-    # This needs to be installed into the package environment, and is slower
-    # than a pre-commit check
-    session.install(".", "pylint")
+    session.run_install(
+        "uv",
+        "sync",
+        "--group=lint",
+        f"--python={session.virtualenv.location}",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
     session.run("pylint", "zeroth", *session.posargs)
 
 
-@nox.session
-def tests(session: nox.Session) -> None:
+# =============================================================================
+# Testing
+
+
+@nox.session(venv_backend="uv")
+def tests(session: nox.Session, /) -> None:
     """Run the unit and regular tests."""
-    session.install(".[test]")
+    session.run_install(
+        "uv",
+        "sync",
+        "--group=test",
+        f"--python={session.virtualenv.location}",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
     session.run("pytest", *session.posargs)
 
 
-@nox.session
-def build(session: nox.Session) -> None:
+# =============================================================================
+
+
+@nox.session(venv_backend="uv")
+def build(session: nox.Session, /) -> None:
     """Build an SDist and wheel."""
     build_path = DIR.joinpath("build")
     if build_path.exists():
         shutil.rmtree(build_path)
 
-    session.install("build")
+    session.run_install(
+        "uv",
+        "sync",
+        "--group=build",
+        f"--python={session.virtualenv.location}",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
     session.run("python", "-m", "build")
