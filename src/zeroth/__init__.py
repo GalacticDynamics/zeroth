a5df88de81c0b487c51cf97f408f467461e43a9e
nstarman <nstarman@users.noreply.github.com>
2025-10-04 17:59:52 -0400

    docs: note about singledispatch
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/zeroth/__init__.py
+++ src/zeroth/__init__.py
@@ -1,6 +1,13 @@
 """Copyright (c) 2024 Nathaniel Starkman. All rights reserved.
 
 zeroth: Efficiently get the index-0 element of an iterable.
+
+Notes
+-----
+- 2025/10: tested using single-dispatch generic function, but it was slower.
+  Currently it's 60.7 ns ± 0.303 ns per loop for `zeroth(list(range(10)))`.
+  Using singledispatch was 232 ns ± 3.44 ns per loop.
+
 """
 
 __all__ = ["zeroth"]
